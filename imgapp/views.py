02a61fd9007518e788d40824e510709327f06c22
Phenodoop <mainulhossain@gmail.com>
2016-09-23 22:36:44 -0600

    generic template

--- imgapp/views.py
+++ imgapp/views.py
@@ -1,34 +1,28 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.template import loader
-from django.http import Http404
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
-#def index(request):
-#    return HttpResponse("Hello, world. You're at the polls index.")
 
-#def index(request):
-#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#    output = ', '.join([p.question_text for p in latest_question_list])
-#    return HttpResponse(output)
+class IndexView(generic.ListView):
+    template_name = 'imgapp/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('imgapp/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'imgapp/detail.html', {'question': question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'imgapp/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'imgapp/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'imgapp/results.html'
 
 def vote(request, question_id):
     p = get_object_or_404(Question, pk=question_id)
