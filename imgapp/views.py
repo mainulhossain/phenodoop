188889c46d2914e2a132dba3678123917bc7d184
Phenodoop <mainulhossain@gmail.com>
2016-09-23 19:33:10 -0600

    template system

--- imgapp/views.py
+++ imgapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
 from django.http import Http404
@@ -22,10 +22,7 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'imgapp/detail.html', {'question': question})
 
 def results(request, question_id):
